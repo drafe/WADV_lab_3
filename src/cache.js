a45f5bb275272f464f9ad873e30c53f7b250a110
drafe <58957966+drafe@users.noreply.github.com>
2022-09-28 11:20:01 +0300

    Lab 3

--- src/cache.js
+++ src/cache.js
@@ -1,4 +1,42 @@
+import {re} from "@babel/core/lib/vendor/import-meta-resolve";
+
+class CacheItem{
+    constructor(_value, _count) {
+        this.value = _value;
+        this.count = _count;
+    }
+}
+
 class Cache{
+    constructor() {
+        this.map = new Map()
+        this.history = new Array()
+    }
+
+    add(key, value, count=1) {
+        this.map.set(key, new CacheItem(value, count))
+    }
+
+    take(key) {
+        const val = this.map.get(key)
+        if ( val && val.count > 0) {
+            val.count -= 1
+            this.history.push([key, val.value, val.count])
+            return this.map.get(key).value
+        }
+        else {
+            this.history.push([key, 'null'])
+            return null;
+        }
+    }
+
+    statistics() {
+        let n = []
+        this.map.forEach((value, key, map) => {
+            n.push([key, value.value, value.count])
+        } )
+        return n
+    }
 
 }
 export {Cache}
--- test/cache.test.js
+++ test/cache.test.js
@@ -1,5 +1,83 @@
 import {Cache} from "../src/cache";
 
-test('it fails', () => {
-    expect(false).toBe(true);
-});
+// test('it fails', () => {
+//     expect(false).toBe(true);
+// });
+
+// test('Create cache', () => {
+//     const cache = new Cache();
+// })
+//
+// test('Add (key, value, count) to cache', () => {
+//     const cache = new Cache();
+//     const key = 'key';
+//     const value = 'value';
+//     const count = 3;
+//     cache.add(key, value, count);
+// })
+//
+// test('Add (key, value) to cache', () => {
+//     const cache = new Cache();
+//     const key = 'key';
+//     const value = 'value';
+//     cache.add(key, value);
+// })
+
+test('Take value by nonexistent key', () => {
+    const cache = new Cache();
+    const not_key = 'not';
+    const key = 'key';
+    const value = 'value';
+    cache.add(key, value);
+    expect(cache.take(key)).toBe(value);
+    expect(cache.take(not_key)).toBeNull();
+})
+
+test('Take value by existent key at once', () => {
+    const cache = new Cache();
+    const key = 'key';
+    const value = 'value';
+    cache.add(key, value);
+    expect(cache.take(key)).toBe(value);
+})
+
+
+test('Take value by existent key twice with 1 access count', () => {
+    const cache = new Cache();
+    const key = 'key';
+    const value = 'value';
+    cache.add(key, value);
+    expect(cache.take(key)).toBe(value);
+    expect(cache.take(key)).toBeNull();
+})
+
+test('Give statistic of cache work', () => {
+    const cache = new Cache();
+    cache.add('1', 'one', 1);
+    cache.add('2', 'two', 2);
+    cache.add('3', 'three', 3);
+
+    cache.take('1');
+    cache.take('2');
+    cache.take('3');
+
+    expect(cache.statistics()).toStrictEqual(
+        [['1', 'one', 0], ['2', 'two', 1], ['3', 'three', 2]]);
+})
+
+test('Give history of cache work', () => {
+    const cache = new Cache();
+    cache.add('1', 'one', 1);
+    cache.add('2', 'two', 2);
+    cache.add('3', 'three', 3);
+
+    cache.take('1');
+    cache.take('1');
+    cache.take('0');
+    cache.take('2');
+    cache.take('2');
+    cache.take('3');
+
+    expect(cache.history).toStrictEqual(
+        [['1', 'one', 0], ['1', 'null'], ['0', 'null'], ['2', 'two', 1], ['2', 'two', 0],['3', 'three', 2]]);
+})
